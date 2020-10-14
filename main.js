31e63dd38cdec2c86deceb1b90bf5fe90a2a934b
跑贼神 <zzx901225@163.com>
2020-10-14 21:46:19 +0800

    first run electron

---
+++ main.js
@@ -0,0 +1,18 @@
+var electron = require('electron')  //引入electron模块
+
+var app = electron.app   // 创建electron引用
+
+var BrowserWindow = electron.BrowserWindow;  //创建窗口引用
+
+var mainWindow = null;  //声明要打开的主窗口
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({ width: 400, height: 400 })   //设置打开的窗口大小
+
+  mainWindow.loadFile('index.html')  //加载那个页面
+
+  //监听关闭事件，把主窗口设置为null
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+
+})
